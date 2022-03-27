a1dd5d759a935bb6d6a277e636d46b081cd8ad2c
Monles <iceueb@gmail.com>
2022-03-27 04:54:20 +0000

    start with Calendar

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Banner from '../components/Banner';
 import SmallCard from '../components/SmallCard';
 import MediumCard from '../components/MediumCard';
 import LargeCard from '../components/LargeCard';
-
+import Footer from '../components/Footer';
 export default function Home({ exploreData, cardsData }) {
 	return (
 		<div className=''>
@@ -44,6 +44,7 @@ export default function Home({ exploreData, cardsData }) {
 					description='Wishlists created by Airbnb'
 					buttonText='Get Inspired'
 				/>
+				<Footer />
 			</main>
 		</div>
 	);
